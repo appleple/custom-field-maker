2e9055bd6a80f34f9b778ee004f8d2ca84c3be77
Hori Godai <ess_president@me.com>
2018-01-17 11:26:31 +0900

    bundle

--- src/components/base.js
+++ src/components/base.js
@@ -381,7 +381,7 @@ export default class Base extends Component {
             <option value="textarea">テキストエリア</option>
             <option value="select">セレクトボックス</option>
             <option value="radio">ラジオボタン</option>
-            <option value="checkbox">チェックボックス</option>
+            {this.constructor.name === 'Field' && <option value="checkbox">チェックボックス</option>}
             <option value="file">ファイル</option>
             <option value="image">画像</option>
           </select>
