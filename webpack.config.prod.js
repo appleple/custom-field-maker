919d180385151cd1efe1e2de7e026f8cdd4d7853
steelydylan <ess_president@me.com>
2018-01-20 22:25:54 +0900

    update production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,46 +5,42 @@ var ExternalsPlugin = webpack.ExternalsPlugin;
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'app'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js']
   },
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-runtime']
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react'],
+            plugins: ['transform-runtime']
+          }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }, {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader'
         }
       }
-    },
-    {
-      test: /\.(scss|css)$/,
-      use: [ 'style-loader', 'css-loader' ]
-    }, {
-      test: /\.(jpg|png)$/,
-      use: {
-        loader: 'url-loader'
-      }
-    }
-  ],
+    ]
+  },
   plugins: [
-    new webpack.DefinePlugin({
-    "process.env": { 
-        NODE_ENV: JSON.stringify("production") 
-      }
-    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
       },
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
+    })
   ],
 }
