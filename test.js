4d32d955e5216e2b90e2216cf282dcf4c161df4d
Hori Godai <ess_president@me.com>
2019-07-03 14:25:22 +0900

    fix test

--- test.js
+++ test.js
@@ -2,10 +2,14 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Field from './src/components/field';
 import FieldConfirmSource from './src/components/field-confirm-source';
+
 const customfield = [{
-  "type": "media",
-  "title": "aaa",
-  "name": "bb",
+  type: 'media',
+  title: 'aaa',
+  name: 'bb',
+  useFocusImage: true,
+  focusImageWidth: 200,
+  focusImageHeight: 200
 }];
 
 describe('custom-field', () => {
