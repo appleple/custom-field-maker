e220ef0c40845271490d83df6e5713367f42594b
宇井 陸登 <ui@appleple.com>
2024-09-02 17:37:44 +0900

    fix: css が npm に公開されない問題の修正

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import CustomFieldMaker from '../../src';
-import '../../src/css/custom-field-maker.css';
+import '../../css/custom-field-maker.css';
 
 const root = createRoot(document.getElementById('app'));
 root.render(
