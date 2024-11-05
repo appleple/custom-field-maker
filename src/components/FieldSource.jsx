70ba17f91b17d8b0e2d4af30f268792f33c468dd
nagatomi <nagatomi.takayuki81@gmail.com>
2024-11-05 15:11:19 +0900

    generatorに修正

--- src/components/FieldSource.jsx
+++ src/components/FieldSource.jsx
@@ -15,3 +15,58 @@ export function FieldSource() {
     </>
   );
 }
+
+// import React, { useEffect, useRef } from 'react';
+// import { Section } from './layouts/Section';
+// import { TableLayout } from './layouts/TableLayout';
+// import { useMakerContext } from '../store/MakerContext';
+
+// export function FieldSource() {
+// 複数のrefをオブジェクトとして管理
+// const refs = {
+//   section: useRef(null),
+//   table: useRef(null)
+// };
+
+// const {
+//   preview: { tag },
+// } = useMakerContext();
+
+// useEffect(() => {
+//   if (window.ACMS && window.ACMS.dispatchEvent) {
+//     if (tag === 'section') {
+//       window.ACMS.dispatchEvent('acmsCustomFieldMakerPreview', refs.section.current, {
+//         item: refs.section.current,
+//       });
+//     }
+//     else if (tag === 'table') {
+//       window.ACMS.dispatchEvent('acmsCustomFieldMakerPreview', refs.table.current, {
+//         item: refs.table.current,
+//       });
+//     }
+//   }
+// }, [refs.section, refs.table]);
+
+// useEffect(() => {
+//   if (tag === 'section') return refs.section.current;
+//   else if (tag === 'table') return refs.table.current;
+//   return null;
+// }, [tag]);
+
+// return (
+//   <>
+//     {() => {
+//       switch (tag) {
+//         case 'section': {
+//           return <Section ref={refs.section} />
+//         }
+//         case 'table': {
+//           return <TableLayout ref={refs.table} />
+//         }
+//         default: return null
+//       }
+//     }
+//     }
+//   </>
+// );
+// }
