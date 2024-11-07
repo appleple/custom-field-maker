ba67da9c9a38a873f5cd3eefa2e9788a4fe079ac
nagatomi <nagatomi.takayuki81@gmail.com>
2024-11-07 11:36:45 +0900

    webpackAnalyzerを削除

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-syntax-dynamic-import'],
+  plugins: ['@babel/plugin-transform-runtime'],
 };
