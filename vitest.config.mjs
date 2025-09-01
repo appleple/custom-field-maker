05c6bb30e5642187dff400dba5b46efb80e80c28
nagatomi <nagatomi.takayuki81@gmail.com>
2025-09-01 15:40:52 +0900

    生成コードのユニットテストができるようにvitestを追加

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'react',
+    include: /\.(jsx?|tsx?)$/,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
