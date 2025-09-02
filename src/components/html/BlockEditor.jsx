7cf3abcf7c814020d0ca9c32cc003c42a293a1b9
nagatomi <nagatomi.takayuki81@gmail.com>
2025-09-02 19:47:00 +0900

    ブロックエディターにバリデーターを追加

--- src/components/html/BlockEditor.jsx
+++ src/components/html/BlockEditor.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useMakerContext } from '../../stores/MakerContext';
 import { OptionValidator } from './OptionValidator';
 import { OptionValidatorFieldGroup } from './OptionValidatorFieldGroup';
+import { OptionNoSearch } from './OptionNoSearch';
 
 export function BlockEditor(props) {
   const { item, isValue = true } = props;
@@ -13,10 +14,21 @@ export function BlockEditor(props) {
     <>
       {mode === 'customfield' && (
         <>
-          <div className="acms-admin-custom-field-content">
+          <div className={{ 'acms-admin-custom-field-content': acmscss }}>
             <div className="js-block-editor" data-target=".js-target" data-html=".js-html">
-              <div className="js-target acms-admin-form-width-full"></div>
-              <input type="hidden" className="js-html" name={item.name} value={`{${item.name}}`} />
+              <div
+                className={{
+                  'js-target': true,
+                  'acms-admin-form-width-full': acmscss,
+                }}
+              ></div>
+              <input
+                type="hidden"
+                className="js-html"
+                name={item.name}
+                value={`{${item.name}}`}
+                {...(jsValidator ? { 'data-validator': item.name } : {})}
+              />
               <input type="hidden" name={`${item.name}:extension`} value="block-editor" />
               <input type="hidden" name="field[]" value={item.name} />
             </div>
@@ -27,30 +39,37 @@ export function BlockEditor(props) {
       )}
 
       {mode === 'fieldgroup' && (
-        <div className="acms-admin-custom-field-content">
-          <div className="js-block-editor" data-target=".js-target" data-html=".js-html">
-            <div className="js-target acms-admin-form-width-full"></div>
-            <input
-              type="hidden"
-              className="js-html"
-              name={`${item.name}[]`}
-              {...(isValue && {
-                defaultValue: `{${item.name}}`,
-              })}
-            />
+        <>
+          <div className="acms-admin-custom-field-content">
+            <div className="js-block-editor" data-target=".js-target" data-html=".js-html">
+              <div className="js-target acms-admin-form-width-full"></div>
+              <input
+                type="hidden"
+                className="js-html"
+                name={`${item.name}[]`}
+                {...(isValue && {
+                  defaultValue: `{${item.name}}`,
+                })}
+              />
+            </div>
           </div>
-        </div>
+          <OptionValidatorFieldGroup item={item} isTemplate={!isValue} />
+        </>
       )}
 
       {mode === 'customunit' && (
-        <div className="acms-admin-custom-field-content">
-          <div className="js-block-editor" data-target=".js-target" data-html=".js-html">
-            <div className="js-target acms-admin-form-width-full"></div>
-            <input type="hidden" className="js-html" name={`${item.name}{id}`} value={`{${item.name}}`} />
-            <input type="hidden" name={`${item.name}{id}:extension`} value="block-editor" />
-            <input type="hidden" name="unit{id}[]" value={`${item.name}{id}`} />
+        <>
+          <div className="acms-admin-custom-field-content">
+            <div className="js-block-editor" data-target=".js-target" data-html=".js-html">
+              <div className="js-target acms-admin-form-width-full"></div>
+              <input type="hidden" className="js-html" name={`${item.name}{id}`} value={`{${item.name}}`} />
+              <input type="hidden" name={`${item.name}{id}:extension`} value="block-editor" />
+              <input type="hidden" name="unit{id}[]" value={`${item.name}{id}`} />
+            </div>
           </div>
-        </div>
+          <OptionValidator item={item} />
+          <OptionNoSearch name={`${item.name}{id}`} noSearch={item.noSearch} />
+        </>
       )}
 
       {mode === 'unitgroup' && (
