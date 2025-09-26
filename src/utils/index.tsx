48d05ab5b082277f0a91310a2b9be89e1a3c3505
nagatomi <nagatomi.takayuki81@gmail.com>
2025-09-26 12:01:17 +0900

    今の実装だとマルチバイトとASCII文字が混合している時にASCII文字も変換されてしまうので、マルチバイト文字だけASCII文字に変換するように修正

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,11 +1,17 @@
 export function generateSafeId(input: string): string {
-  // マルチバイトでない場合はそのまま返す
-  if (input === encodeURIComponent(input)) {
-    return input;
-  }
-
-  return btoa(encodeURI(encodeURIComponent(input)))
-    .replace(/=/g, '') // = 記号を除去
-    .replace(/\+/g, '-') // + をハイフンに置換
-    .replace(/\//g, '_'); // / をアンダースコアに置換
+  return Array.from(input)
+    .map((ch) => {
+      if (ch.charCodeAt(0) < 128) {
+        // ASCII はそのまま
+        return ch;
+      } else {
+        // 日本語など非ASCIIだけエンコード
+        const encoded = btoa(encodeURIComponent(ch))
+          .replace(/=/g, '') // = を除去
+          .replace(/\+/g, '-') // + → -
+          .replace(/\//g, '_'); // / → _
+        return encoded;
+      }
+    })
+    .join('');
 }
